80d1f6027cc361144aad5fef996a179ec9b701c8
KinzzoCS <linkbomt@gmail.com>
2026-06-05 23:26:40 -1200

    Add firebase config for GitHub Pages deployment

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBgJgQIb0TzK9g7JLov97GJRaM5PDo7g5o",
+  authDomain: "mineros-en-vivo-161.firebaseapp.com",
+  projectId: "mineros-en-vivo-161",
+  storageBucket: "mineros-en-vivo-161.firebasestorage.app",
+  messagingSenderId: "284609016326",
+  appId: "1:284609016326:web:1b7d789d534b57ca32c987"
+};
+
+export const firebaseReady = !firebaseConfig.apiKey.includes("REEMPLAZA");
